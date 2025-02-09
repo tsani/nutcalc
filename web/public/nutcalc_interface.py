5bf3531a0b3f420db3f8ffff564d290d7f82182d
Jacob Errington <git@mail.jerrington.me>
2025-02-09 10:33:03 -0500

    nutcalc_interface: remove logs, unify output data of load-module and eval

--- web/public/nutcalc_interface.py
+++ web/public/nutcalc_interface.py
@@ -8,9 +8,9 @@ from dataclasses import dataclass
 class Nutcalc:
     def __init__(self):
         self.output_stream = StringIO()
-        self.interpreter = nutcalc.interpret.Interpreter(output_stream=self.output_stream)   
+        self.interpreter = nutcalc.interpret.Interpreter(output_stream=self.output_stream)
         self.continuations = {}
-        
+
     def continuation(self, name):
         def wrapper(f):
             def wrapped(*args, **kwargs):
@@ -19,7 +19,7 @@ class Nutcalc:
             wrapped.__name__ = f'<continuation "{name}" {f.__name__}>'
             self.continuations[name] = wrapped
         return wrapper
-        
+
     def dispatch(self, request):
         request = dict(request)
         print(f'received request {request}')
@@ -35,7 +35,6 @@ class Nutcalc:
             case { 'type': 'eval', 'contents': line }:
                 try:
                     self._eval_nutcalc(line)
-                    print('hi')
                     output = self._collect_output()
                 except Exception as e:
                     respond_to(request, {
@@ -43,7 +42,6 @@ class Nutcalc:
                         'error': e.toString() if 'toString' in dir(e) else str(e),
                     })
                 else:
-                    print('oh')
                     respond_to(request, {
                         'success': True,
                         'data': output,
@@ -51,26 +49,26 @@ class Nutcalc:
 
             case { 'type': 'load-module', 'name': name, 'contents': contents }:
                 self._load_root_module(request, name, contents)
-            
+
             case _:
                 respond_to(request, {
                     'success': False,
                     'error': f'unhandled request type {request['type']}',
                 })
-                
+
     def _load_root_module(self, originator, name, contents):
         """Loads a module identified as a 'root module', i.e. the root of a DAG of
         modules to ultimately be loaded."""
-        
+
         # Modules that are currently being loaded
         # This is a 'visited set', used to detect import loops.
         loading = set()
-        
+
         # This is a continuation-based implementation of async module loading.
 
         # `load_module` implements the DFS through the DAG of modules as they're
         # being parsed. Once all of a module's imports are loaded into the
-        # interpreter, then the module itself is loaded. 
+        # interpreter, then the module itself is loaded.
         # Whenever `load_module` needs to get the contents of a as-of-yet unknown
         # module, it yields its name and waits for someone to .send() it the
         # contents of that module.
@@ -87,10 +85,10 @@ class Nutcalc:
                 yield from _load_module(imp.path, new_contents)
             self.interpreter.load_module(name, module)
             loading.remove(name)
-            
+
         # lm_it therefore emits a sequence of module names needing to be loaded
         lm_it = load_module(name, contents)
-        
+
         # feed_module_contents(x) sends `x` as the contents of the last requested
         # module into the recursive `load_module` loop. The initial `x` has to be
         # None to kickstart the process.
@@ -102,7 +100,7 @@ class Nutcalc:
                 output = self._collect_output()
                 respond_to(originator, {
                     'success': True,
-                    'data': { 'output': output },
+                    'data': output,
                 })
             except nutcalc.NutcalcError as e:
                 respond_to(originator, {
@@ -121,7 +119,7 @@ class Nutcalc:
                 self._continue(module_path, feed_module_contents)
 
         feed_module_contents(None)
-                
+
     def _continue(self, name, function):
         def continuation(response):
             del self.continuations[name]
@@ -131,17 +129,17 @@ class Nutcalc:
     def _eval_nutcalc(self, line):
         stmt = nutcalc.parser.parse_stmt(line)
         self.interpreter.execute(stmt)
-        
+
     def _collect_output(self):
         self.output_stream.seek(0)
         output = self.output_stream.read()
         self.output_stream.truncate(0)
         return output
-    
+
 @bind(document, 'nutcalc_to')
 def handle_nutcalc_request(e):
     NUTCALC.dispatch(e.detail)
-    
+
 @bind(document, 'nutcalc_reset')
 def handle_nutcalc_reset(*e):
     global NUTCALC
@@ -153,7 +151,7 @@ def respond_to(message, data):
         'type': 'response',
         'of': message['id'],
         'data': data
-    }) 
+    })
 
 def emit(message, key='nutcalc_from'):
     document.dispatchEvent(
@@ -161,18 +159,18 @@ def emit(message, key='nutcalc_from'):
             'detail': message,
         }),
     )
-    
+
 @dataclass
 class ModuleLoopError(nutcalc.NutcalcError):
     location: str
     module_name: str
     loading_set: set[str]
-        
+
     def __str__(self):
         return f'{self.location}: loading {self.module_name} causes an import ' \
             f'loop among already loaded modules {str(self.loading_set)}'
-    
-NUTCALC = True
+
+NUTCALC = None
 handle_nutcalc_reset()
 
 print('nutcalc interface loaded')
